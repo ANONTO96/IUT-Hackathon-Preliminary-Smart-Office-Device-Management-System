74a68d3875bc86a130a3a615120bdd1ff6d59669
ANONTO <tanjirulanonto04@gmail.com>
2026-07-03 23:25:12 +0600

    react,daisyUI and Tailwind Css for frontend installed

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center">
+       getting started with react
+      </div>
+    </>
+  )
+}
+
+export default App
